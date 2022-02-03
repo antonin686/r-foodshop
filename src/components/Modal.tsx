67ff1815857956c7eaba9ff125ba9781368ed635
Md.Antonin Islam <md.antonin6862@gmail.com>
2022-02-04 03:59:56 +0600

    Product Card Updated + Modal added

---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+import Backdrop from "./Backdrop";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+
+
+function Modal({ handleClose, children }: any) {
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="px-2"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        {children}
+      </motion.div>
+    </Backdrop>
+  );
+}
+
+export default Modal;
